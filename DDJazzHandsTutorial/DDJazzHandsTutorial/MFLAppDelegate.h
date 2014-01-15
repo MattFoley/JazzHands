1be6cc42d7e016dbbdbc9ea477c43938feec674b
teejay <tfallon@mail.depaul.edu>
2014-01-15 09:28:06 -0600

    Adding files.

---
+++ DDJazzHandsTutorial/DDJazzHandsTutorial/MFLAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  MFLAppDelegate.h
+//  DDJazzHandsTutorial
+//
+//  Created by teejay on 1/14/14.
+//  Copyright (c) 2014 MFL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DDTutorialViewController.h"
+
+@interface MFLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) DDTutorialViewController *tutorialViewController;
+@end
---
+++ DDJazzHandsTutorial/DDJazzHandsTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DDJazzHandsTutorial
+//
+//  Created by teejay on 1/14/14.
+//  Copyright (c) 2014 MFL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MFLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFLAppDelegate class]));
+    }
+}
